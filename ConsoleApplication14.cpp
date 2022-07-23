6f80adb289c84f55b4dcdd2824cac74d172399d1
injoon149 <dlswns8679@naver.com>
2022-07-23 20:53:38 +0900

    초기 커밋.

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+
+	cout.tie(NULL);
+
+	ios::sync_with_stdio(false);
+	int a = 0;
+	cin >> a;
+	int num = 0;
+	int array[10001] = { 0, };
+	for (int i = 0; i < a; i++)
+	{
+		cin >> num;
+		array[num]++;
+	}
+	for (int i = 0; i < 10001;)
+	{
+		if (array[i] == 0)
+		{
+			i++;
+			continue;
+		}
+		else {
+			cout << i << '\n';
+			array[i]--;
+		}
+	}
+
+	return 0;
+
+
+
+}
